5a2f53986311bdb7067c6c6e690d83a07f426211
Ruud Renssen <ruud@vicompany.nl>
2021-10-08 10:02:13 +0200

    create day based countdown

--- main.js
+++ main.js
@@ -20,16 +20,45 @@ const data = [
 			},
 			{
 				name: 'Grand Prix',
-				start: new Date(1636549200000)
+				start: new Date(1633867200000)
 			}
 		]
 	}
 ]
 
+const ms = 1;
+const sec = 1000 * ms;
+const min = 60 * sec;
+const hour = 60 * min;
+const day = 24 * hour;
+const units = [day, hour, min, sec, ms];
+
+const countdown = function (value = 0) {
+	let i =  0;
+	let t = [];
+	let u = [...units]
+
+	do {
+		// distribute value over days, hours, minutes and seconds
+		if (value >= u[0]) {
+			value -= u[0];
+			i++;
+		} else {
+			u.shift();
+			t.push(i);
+			i = 0;
+		}
+	} while (value);
+
+	return t;
+}
+
 setInterval(() => {
 	data.forEach(value => {
 		value.sessions.forEach(session => {
-			const countdown = new Date(session.start - Date.now()); // time to session in ms
+			const time = new Date(session.start - Date.now()); // time to session in ms
+			console.log(countdown(time));
 		});
 	});
 }, 1000);
+
