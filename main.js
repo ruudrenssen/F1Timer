3d5c6198bcb3df1b49187783caabefeeda98f50f
Ruud Renssen <ruud@vicompany.nl>
2021-10-09 06:51:33 +0200

    refine

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const data = [
 	{
-		title: "Turkish Grand Prix",
+		title: 'Turkish Grand Prix',
+		subtitle: 'Formula 1 Rolex Turkey Grand Prix 2021',
 		sessions: [
 			{
-				name: 'Free Practice 1',
+				name: 'Practice 1',
 				start: new Date(1633681800000)
 			},
 			{
-				name: 'Free Practice 2',
+				name: 'Practice 2',
 				start: new Date(1633694400000)
 			},
 			{
-				name: 'Free Practice 3',
+				name: 'Practice 3',
 				start: new Date(1633770000000)
 			},
 			{
@@ -19,8 +20,34 @@ const data = [
 				start: new Date(1633780800000)
 			},
 			{
-				name: 'Grand Prix',
+				name: 'Race',
 				start: new Date(1633867200000)
+			}
+		]
+	},
+	{
+		title: 'United States',
+		subtitle: 'Formula 1 Aramco United States Grand Prix 2021',
+		sessions: [
+			{
+				name: 'Practice 1',
+				start: new Date(1634920200000)
+			},
+			{
+				name: 'Practice 2',
+				start: new Date(1634932800000)
+			},
+			{
+				name: 'Practice 3',
+				start: new Date(1635012000000)
+			},
+			{
+				name: 'Qualifying',
+				start: new Date(1635022800000)
+			},
+			{
+				name: 'Race',
+				start: new Date(1635102000000)
 			}
 		]
 	}
@@ -33,6 +60,13 @@ const hour = 60 * min;
 const day = 24 * hour;
 const units = [day, hour, min, sec, ms];
 const decimals = [1, 2, 2, 2, 3]
+
+const calEl = document.querySelector('[data-module="gp-calendar"]');
+const gpTemplate = document.querySelector('[data-module="gp-item"]').cloneNode(true);
+const sessionTemplate = document.querySelector('[data-module="gp-session"]').cloneNode(true);
+
+document.querySelector('[data-module="gp-session"]').remove();
+document.querySelector('[data-module="gp-item"]').remove();
 
 const countdown = function(value = 0) {
 	return units.map((unit) => {
@@ -48,15 +82,41 @@ const countdown = function(value = 0) {
 	});
 }
 
-const placeholders = document.querySelectorAll('[data-module="time"]');
+const updateCountdown = function (el) {
+	const start = new Date(el.dataset.start);
+	const slots = el.querySelectorAll('[data-module="gp-time"]');
+	const time = countdown(new Date(start - Date.now())); // time to session in ms
+
+	if(start - Date.now() > 0) {
+		slots.forEach((slot, i) => {
+			slot.textContent = time[i].toString().padStart(decimals[i], '0');
+		});
+	} else {
+		el.remove();
+	};
+}
+
+data.forEach(gp => {
+	const el = gpTemplate.cloneNode(true);
+	gp.sessions.forEach(session => {
+		const e = sessionTemplate.cloneNode(true);
+
+		e.dataset.start = session.start;
+		e.querySelector('h3').textContent = session.name;
+		updateCountdown(e);
+		e.hidden = false;
+		el.querySelector('[data-module="gp-sessions"]').appendChild(e);
+	});
+
+	el.querySelector('h1').textContent = gp.title;
+	el.querySelector('h2').textContent = gp.subtitle;
+	el.hidden = false;
+
+	calEl.appendChild(el);
+});
 
 setInterval(() => {
-	data.forEach(value => {
-		value.sessions.forEach(session => {
-			const time = countdown(new Date(session.start - Date.now())); // time to session in ms
-			placeholders.forEach((placeholder, i) => {
-				placeholder.textContent = time[i].toString().padStart(decimals[i], '0');
-			});
-		});
-	});
+	document.querySelectorAll('[data-module="gp-session"]').forEach(el => {
+		updateCountdown(el);
+	})
 }, 24);
